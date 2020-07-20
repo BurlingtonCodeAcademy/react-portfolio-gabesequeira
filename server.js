80af3ebe940ca67992cdabddc6f608d69ed1b21e
Gabe Sequeira-Bacher <gabesequeirabacher@gmail.com>
2020-07-20 09:20:06 -0400

    server to handle all routes

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.static(path.resolve('./client/build')))
+
+app.get('*', (req, res) => {
+    res.sendFile('index.html')
+})
+
+app.listen(port)
