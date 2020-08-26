3f38ba24925d3d5a427a2301c541bbd3164ae9db
Gabe Sequeira-Bacher <gxs11260@vsc.edu>
2020-08-26 09:39:29 -0400

    hobbies description

--- client/src/JS/Hobbies.js
+++ client/src/JS/Hobbies.js
@@ -31,7 +31,7 @@ class Hobbies extends React.Component {
                 ></iframe>
               </div>
               <div id='paragraph'>
-              <p>One of my hobbies is music. I have been playing the organ since January of 2020. I began learning Web Development in March of that year. I have a B.A. in Music from Castleton University. Here is a clip of myself practicing the organ at Christ Church in Montpelier, VT, July 2020.</p>
+              <p>One of my hobbies is music. I started playing the organ in January of the year 2020. I didn't start learning Web Development until about a month later. I obtained my B.A. in music from Castleton University. Here is a clip of myself practicing the organ at Christ Church in Montpelier, VT, July 2020.</p>
               </div>
             </div>
           </div>
